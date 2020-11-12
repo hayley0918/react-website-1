f071bc0974c89a5bbcd915d357f56c669b3f59f5
Min Kyung CHOI <haleychoi0918@gmail.com>
2020-11-13 07:35:39 +1100

    add font awesome link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import './App.css'
 import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Services from './components/pages/Services'
 import Products from './components/pages/Products'
 import SignUp from './components/pages/SignUp'
+import './App.css'
 
 function App() {
   return (
@@ -14,9 +14,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/services" exact component={Services} />
-          <Route path="/products" exact component={Products} />
-          <Route path="/sign-up" exact component={SignUp} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Products} />
+          <Route path="/sign-up" component={SignUp} />
         </Switch>
       </Router>
     </>
